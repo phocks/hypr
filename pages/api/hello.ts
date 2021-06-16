35236b181088c4eaadca3cdd758025ab6257ac01
Joshua Byrd <phocks@gmail.com>
2021-06-16 14:46:09 +1000

    Encrypt text and email

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+const CryptoJS = require("crypto-js");
 const ses = require("node-ses");
 const client = ses.createClient({
   key: process.env.SES_KEY,
@@ -7,23 +8,37 @@ const client = ses.createClient({
   amazon: "https://email.us-east-2.amazonaws.com",
 });
 
-console.log(process.env.DB_HOST);
+// Convert callback to promise for simplicity
+const sendEmailPromise = (options) => {
+  return new Promise((resolve, reject) => {
+    client.sendEmail(options, (err, emailData, emailRes) => {
+      if (err) return reject(err);
+      resolve(emailData);
+    });
+  });
+};
 
-export default (req, res) => {
-  client.sendEmail(
-    {
-      to: "phocks@gmail.com",
-      from: '"Hypr Support" <info@hypr.gq>',
-      subject: "Your code",
-      message: "Hello there.",
-      altText: `Your OTP is: 7463`,
-    },
-     (err, emailData, emailRes) => {
-      console.log(err);
+// Encrypt
+const ciphertext = CryptoJS.AES.encrypt(
+  "phocks@gmail.com",
+  process.env.EMAIL_LOGIN_ENCRYPTION_MASTER_KEY
+).toString();
 
-      // TODO: Make this a Promise
-      res.statusCode = 200;
-      res.json({ name: "Joshua Byrd" });
-    }
-  );
+// Decrypt
+var bytes = CryptoJS.AES.decrypt(ciphertext, process.env.EMAIL_LOGIN_ENCRYPTION_MASTER_KEY);
+var originalText = bytes.toString(CryptoJS.enc.Utf8);
+
+console.log(originalText); // 'my message'
+
+export default async (req, res) => {
+  await sendEmailPromise({
+    to: "phocks@gmail.com",
+    from: '"Hypr Support" <info@hypr.gq>',
+    subject: "Your code",
+    message: ciphertext + " " + originalText,
+    altText: `Your OTP is: 7463`,
+  });
+
+  res.statusCode = 200;
+  res.json({ message: "Email has been sent..." });
 };
