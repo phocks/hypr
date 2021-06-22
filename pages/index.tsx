95734a0b42c1fa2590eeeeded1d1c88241bc2584
Joshua Byrd <phocks@gmail.com>
2021-06-23 08:51:46 +1000

    Login email

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
 import Head from "next/head";
+import { useState } from "react";
+import { Button, TextField } from "@material-ui/core";
+import { Formik } from "formik";
+
 import styles from "../styles/Home.module.css";
 
 function Home() {
+  const [isLoggedIn, setIsLockedIn] = useState(false);
+
+  const handleLoginSubmit = async (event) => {
+    console.log(event);
+    event.preventDefault();
+  };
+
   return (
     <div className="flex-container">
       <Head>
@@ -9,7 +20,38 @@ function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Hypr</h1>
+      <Formik
+        initialValues={{ email: "" }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+          ...theRest
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <TextField
+              name="email"
+              type="email"
+              label="login email"
+              onChange={handleChange}
+              value={values.email}
+              onBlur={handleBlur}
+            />
+          </form>
+        )}
+      </Formik>
     </div>
   );
 }
