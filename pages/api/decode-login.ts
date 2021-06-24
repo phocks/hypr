ed6715ddac8f37f461fa8cfa131c590c15c3ddbf
Joshua Byrd <phocks@gmail.com>
2021-06-24 18:25:42 +1000

    Kinda working almost

--- pages/api/decode-login.ts
+++ pages/api/decode-login.ts
@@ -1,24 +1,25 @@
 const CryptoJS = require("crypto-js");
 
-export default async (req, res) => {
+export default (req, res) => {
   if (req.method === "POST") {
     const body = req.body;
     const loginEncrypted = decodeURIComponent(body.login);
-
-    console.log(body);
-    console.log(loginEncrypted)
 
     const bytes = CryptoJS.AES.decrypt(
       loginEncrypted,
       process.env.EMAIL_LOGIN_ENCRYPTION_MASTER_KEY
     );
 
-    const decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+    // decryptedData should have an email and a time property
+    try {
+      const decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
 
-    console.log(decryptedData);
-
-    res.statusCode = 200;
-    res.json({ message: "Done..." });
+      res.statusCode = 200;
+      res.json({ message: `Email logged in ${decryptedData.email}` });
+    } catch (e) {
+      res.statusCode = 500;
+      res.json({ message: "Invalid login detected..." });
+    }
   } else {
     res.statusCode = 200;
     res.json({ message: "POST only..." });
