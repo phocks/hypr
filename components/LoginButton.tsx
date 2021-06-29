333ba077ed09b38c1cc0097ab9c1fc1667a45c47
Joshua Byrd <phocks@gmail.com>
2021-06-29 18:15:09 +1000

    ok

---
+++ components/LoginButton.tsx
@@ -0,0 +1,21 @@
+import { Button } from "@material-ui/core";
+import useSWR from "swr";
+
+const LoginButton = () => {
+  const fetcher = (args) => fetch(args).then((res) => res.json());
+  const { data, error } = useSWR("/api/login", fetcher);
+
+  console.log(data);
+
+  const handleLogin = (event) => {
+    console.log("clicked");
+  };
+
+  return (
+    <Button onClick={handleLogin} variant="outlined">
+      Login
+    </Button>
+  );
+};
+
+export default LoginButton;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,37 @@
 import Head from "next/head";
 import { useState, useEffect } from "react";
-import LoginForm from "../components/LoginForm";
 import useSWR from "swr";
 import { useRouter } from "next/router";
+
+// Local components
+import LoginForm from "../components/LoginForm";
+import LoginButton from "../components/LoginButton";
 
 import styles from "../styles/Home.module.css";
 
 const Home = (props) => {
-  const [isLoggedIn, setIsLockedIn] = useState(false);
-  const router = useRouter();
+  // const [isLoggedIn, setIsLockedIn] = useState(false);
+  // const router = useRouter();
 
-  const postFetcher = (url) =>
-    fetch(url, {
-      body: JSON.stringify({
-        login: router.query.login,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-      method: "POST",
-    }).then((r) => r.json());
+  // const postFetcher = (url) =>
+  //   fetch(url, {
+  //     body: JSON.stringify({
+  //       login: router.query.login,
+  //     }),
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     method: "POST",
+  //   }).then((r) => r.json());
 
-  const shouldFetch = typeof router.query.login !== "undefined";
+  // const shouldFetch = typeof router.query.login !== "undefined";
 
-  const { data, error } = useSWR(
-    shouldFetch ? "/api/decode-login" : null,
-    postFetcher
-  );
+  // const { data, error } = useSWR(
+  //   shouldFetch ? "/api/decode-login" : null,
+  //   postFetcher
+  // );
 
-  if (data) console.log(data)
+  // if (data) console.log(data);
 
   return (
     <div className="flex-container">
@@ -37,7 +40,7 @@ const Home = (props) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <LoginForm />
+      <LoginButton />
     </div>
   );
 };
