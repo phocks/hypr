a14daef653ec852167d5b6841651a500c78a1e25
Joshua Byrd <phocks@gmail.com>
2021-06-30 10:18:33 +1000

    Commit

--- components/LoginButton.tsx
+++ components/LoginButton.tsx
@@ -3,12 +3,14 @@ import useSWR from "swr";
 
 const LoginButton = () => {
   const fetcher = (args) => fetch(args).then((res) => res.json());
+  
   const { data, error } = useSWR("/api/login", fetcher);
-
   console.log(data);
+  
 
   const handleLogin = (event) => {
     console.log("clicked");
+    
   };
 
   return (
