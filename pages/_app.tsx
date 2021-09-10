7effd95e09076583037d55cd98a21d4535d48644
Joshua Byrd <phocks@gmail.com>
2021-09-10 20:58:38 +1000

    Dark mode out of the box!

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,24 @@
 import "../styles/globals.scss";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  initialColorMode: "light",
+  useSystemColorMode: false,
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+};
+
+const theme = extendTheme({ colors });
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,21 @@
 import { useState, useEffect } from "react";
+import { useColorMode, Container, Button } from "@chakra-ui/react";
 
-import styles from '../styles/Home.module.scss'
+import styles from "../styles/Home.module.scss";
 
-export default function Home() {
+function Home() {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   useEffect(() => {}, []);
 
   return (
     <div className={styles.container}>
-      Hello world!
+      <Button onClick={toggleColorMode}>
+        Toggle {colorMode === "light" ? "Dark" : "Light"}
+      </Button>
+      <Container>Hello world!</Container>
     </div>
   );
 }
+
+export default Home;
