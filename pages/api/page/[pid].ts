90fc57803e205e5d674c04c2d9ff6e33f74c50be
Joshua Byrd <phocks@gmail.com>
2021-09-13 08:13:35 +1000

    Yeah good

--- pages/api/page/[pid].ts
+++ pages/api/page/[pid].ts
@@ -1,4 +1,31 @@
-export default function handler(req, res) {
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
+
+const page = async (req, res) => {
   const { pid } = req.query;
-  res.status(200).json({ text: "Page text" });
-}
+
+  if (!pid) {
+    res.status(400).json({
+      error: "No pid provided",
+    });
+    return;
+  }
+
+  const { data, error } = await supabase
+    .from("links")
+    .select()
+    .eq("id", pid)
+    .single();
+
+  if (error) {
+    res.status(500).send({ error: "Something failed!" });
+    return;
+  }
+
+  res.status(200).json(data);
+};
+export default page;
--- pages/page/[pid].tsx
+++ pages/page/[pid].tsx
@@ -5,9 +5,11 @@ import useSWR from "swr";
 import { Skeleton } from "@chakra-ui/react";
 
 const usePage = (pid) => {
-  const { data, error } = useSWR(`/api/page/${pid}`, fetcher);
-
-  console.log(data);
+  const shouldFetch = pid !== undefined;
+  const { data, error } = useSWR(
+    shouldFetch ? `/api/page/${pid}` : null,
+    fetcher
+  );
 
   return {
     data: data,
@@ -16,15 +18,27 @@ const usePage = (pid) => {
   };
 };
 
-const Post = () => {
-  const router = useRouter();
-  const { pid } = router.query;
-
+const Text = ({ pid }) => {
   const { data, isLoading, isError } = usePage(pid);
   if (isLoading) return <Skeleton height={6} />;
   if (isError) return <div>failed to load</div>;
 
-  return <Layout>hello: {data.text}</Layout>;
+  return data.text;
+};
+
+const Post = () => {
+  const router = useRouter();
+  const { pid } = router.query;
+
+  // const { data, isLoading, isError } = usePage(pid);
+  // if (isLoading) return <Skeleton height={6} />;
+  // if (isError) return <div>failed to load</div>;
+
+  return (
+    <Layout>
+      <Text pid={pid} />
+    </Layout>
+  );
 };
 
 export default Post;
